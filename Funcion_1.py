81c7dbd835c05b86033f71281e734811e6742f4d
SilentGee03 <a01736213@tec.mx>
2024-10-31 15:40:45 -0600

    Creación de rama frontend

--- Funcion_1.py
+++ Funcion_1.py
@@ -3,3 +3,9 @@ print('Comenzar con las modificaciones')
 a = 45 
 b = 56
 print(a+b)
+
+# Crear las funcioens necesarias para el frontend
+
+def imprimir():
+    print('Esta es la rama para desarrollar el frontend')
+imprimir()
